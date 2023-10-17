959fdd895e59bb3afb3bbb391cf67b101320a96d
Max <max.marroquin0@gmail.com>
2023-10-16 22:14:25 -0600

    Simulación de Memoria y Paginación y Políticas de Manejo de Memoria

--- proyecto_memoria.cpp
+++ proyecto_memoria.cpp
@@ -9,36 +9,44 @@
 #include <unordered_map>
 #include <list>
 
-enum FileType {
+enum FileType
+{
     TEXT,
     BINARY
 };
 
-enum AllocationPolicy {
+enum AllocationPolicy
+{
     BEST_FIT,
-    WORST_FIT
+    WORST_FIT,
+    FIFO,
+    LRU
 };
 
-class MemoryBlock {
+class MemoryBlock
+{
 public:
     int startAddress;
     int size;
     std::string fileID;
     std::string content;
 
-    MemoryBlock(int startAddress, int size, const std::string& fileID, const std::string& content)
+    MemoryBlock(int startAddress, int size, const std::string &fileID, const std::string &content)
         : startAddress(startAddress), size(size), fileID(fileID), content(content) {}
 
-    void assign() {
+    void assign()
+    {
         std::cout << "Asignando bloque para " << fileID << std::endl;
     }
 
-    void release() {
+    void release()
+    {
         std::cout << "Liberando bloque de " << fileID << std::endl;
     }
 };
 
-class MemoryManager {
+class MemoryManager
+{
 public:
     int totalSize;
     int pageSize;
@@ -51,49 +59,102 @@ public:
     MemoryManager(int totalSize, int pageSize, AllocationPolicy policy)
         : totalSize(totalSize), pageSize(pageSize), policy(policy) {}
 
-    void loadFile(const std::string& fileName, FileType fileType) {
+    void loadFile(const std::string &fileName, FileType fileType)
+    {
         std::ifstream file;
 
-        if (fileType == TEXT) {
+        if (fileType == TEXT)
+        {
             file.open(fileName, std::ios::in);
-        } else {
+        }
+        else
+        {
             file.open(fileName, std::ios::in | std::ios::binary);
         }
 
-        if (!file) {
+        if (!file)
+        {
             std::cerr << "No se puede abrir el archivo: " << fileName << std::endl;
             return;
         }
 
         std::string content = std::string((std::istreambuf_iterator<char>(file)),
-                              std::istreambuf_iterator<char>());
+                                          std::istreambuf_iterator<char>());
         int fileSize = content.size();
         int requiredPages = (fileSize + pageSize - 1) / pageSize;
         int requiredSize = requiredPages * pageSize;
 
         int startAddress = findStartAddress(requiredSize);
 
-        if (startAddress + requiredSize > totalSize) {
+        if (startAddress + requiredSize > totalSize)
+        {
             std::cout << "No hay suficiente memoria para " << fileName << std::endl;
             return;
         }
 
         MemoryBlock newBlock(startAddress, requiredSize, fileName, content);
         newBlock.assign();
-        blocks.push_back(newBlock);
-        pageQueue.push(newBlock);
+
+        if (policy == BEST_FIT)
+        {
+            int bestStart = findBestFit(requiredSize);
+            blocks.push_back(newBlock);
+        }
+        else if (policy == WORST_FIT)
+        {
+            int worstStart = findWorstFit(requiredSize);
+            blocks.push_back(newBlock);
+        }
+        else if (policy == FIFO)
+        {
+            if (pageQueue.size() >= totalSize / pageSize)
+            {
+                FIFOPageReplacement(newBlock);
+            }
+            else
+            {
+                pageQueue.push(newBlock);
+            }
+        }
+        else if (policy == LRU)
+        {
+            if (pageQueue.size() >= totalSize / pageSize)
+            {
+                LRUPageReplacement(newBlock);
+            }
+            else
+            {
+                pageQueue.push(newBlock);
+            }
+        }
+        else
+        {
+            blocks.push_back(newBlock);
+            pageQueue.push(newBlock);
+        }
+
+        if (blocks.size() > 1 && (float)blocks.size() / totalSize > 0.8)
+        {
+            defragment();
+        }
 
         accessPage(fileName);
     }
 
-    void accessPage(const std::string& fileID) {
-        if (lruMap.find(fileID) != lruMap.end()) {
+    void accessPage(const std::string &fileID)
+    {
+        if (lruMap.find(fileID) != lruMap.end())
+        {
             lruList.erase(lruMap[fileID]);
             lruList.push_front(*lruMap[fileID]);
             lruMap[fileID] = lruList.begin();
-        } else {
-            for (const auto& block : blocks) {
-                if (block.fileID == fileID) {
+        }
+        else
+        {
+            for (const auto &block : blocks)
+            {
+                if (block.fileID == fileID)
+                {
                     lruList.push_front(block);
                     lruMap[fileID] = lruList.begin();
                     break;
@@ -102,32 +163,126 @@ public:
         }
     }
 
-    void replacePageLRU(const std::string& newFileID) {
-        if (lruList.size() == blocks.size()) {
+    void divideAndStoreBlocks()
+    {
+        int numPages = totalSize / pageSize;
+        for (int i = 0; i < numPages; ++i)
+        {
+            MemoryBlock newBlock(i * pageSize, pageSize, "", "");
+            blocks.push_back(newBlock);
+        }
+    }
+
+    void defragment()
+    {
+        std::sort(blocks.begin(), blocks.end(), [](const MemoryBlock &a, const MemoryBlock &b)
+                  { return a.startAddress < b.startAddress; });
+
+        std::vector<MemoryBlock> newBlocks;
+        int currentAddress = 0;
+
+        for (const auto &block : blocks)
+        {
+            if (block.startAddress != currentAddress)
+            {
+                int gapSize = block.startAddress - currentAddress;
+                MemoryBlock newGap(currentAddress, gapSize, "", "");
+                newBlocks.push_back(newGap);
+                currentAddress += gapSize;
+            }
+            newBlocks.push_back(block);
+            currentAddress += block.size;
+        }
+
+        blocks = newBlocks;
+    }
+
+    void deleteBlock(int startAddress)
+    {
+        auto it = std::remove_if(blocks.begin(), blocks.end(), [startAddress](const MemoryBlock &block)
+                                 { return block.startAddress == startAddress; });
+
+        if (it != blocks.end())
+        {
+            it->release(); // Liberar el bloque
+            blocks.erase(it, blocks.end());
+            std::cout << "Bloque en dirección " << startAddress << " eliminado.\n";
+        }
+        else
+        {
+            std::cout << "Bloque en dirección " << startAddress << " no encontrado.\n";
+        }
+    }
+
+    void overwriteBlock(int startAddress, const std::string &newContent)
+    {
+        auto it = std::find_if(blocks.begin(), blocks.end(), [startAddress](const MemoryBlock &block)
+                               { return block.startAddress == startAddress; });
+
+        if (it != blocks.end())
+        {
+            it->content = newContent;
+            std::cout << "Bloque en dirección " << startAddress << " sobrescrito.\n";
+        }
+        else
+        {
+            std::cout << "Bloque en dirección " << startAddress << " no encontrado para sobrescribir.\n";
+        }
+    }
+
+    void FIFOPageReplacement(const MemoryBlock &newBlock)
+    {
+        MemoryBlock oldestBlock = pageQueue.front();
+        pageQueue.pop();
+        blocks[oldestBlock.startAddress / pageSize] = newBlock;
+        pageQueue.push(newBlock);
+        oldestBlock.release();
+    }
+
+    void LRUPageReplacement(const MemoryBlock &newBlock)
+    {
+        MemoryBlock leastRecentlyUsed = pageQueue.front();
+        pageQueue.pop();
+        blocks[leastRecentlyUsed.startAddress / pageSize] = newBlock;
+        pageQueue.push(newBlock);
+        leastRecentlyUsed.release(); // Release the least recently used block
+    }
+
+    void replacePageLRU(const std::string &newFileID)
+    {
+        if (lruList.size() == blocks.size())
+        {
             MemoryBlock lastUsedPage = lruList.back();
             lruList.pop_back();
             lruMap.erase(lastUsedPage.fileID);
 
             deleteFile(lastUsedPage.fileID);
-            loadFile(newFileID, TEXT);  // Cambiar TEXT a BINARY según sea necesario
+            loadFile(newFileID, TEXT); // Cambiar TEXT a BINARY según sea necesario
         }
     }
 
-    int findStartAddress(int requiredSize) {
-        if (policy == BEST_FIT) {
+    int findStartAddress(int requiredSize)
+    {
+        if (policy == BEST_FIT)
+        {
             return findBestFit(requiredSize);
-        } else {
+        }
+        else
+        {
             return findWorstFit(requiredSize);
         }
     }
 
-    int findBestFit(int requiredSize) {
+    int findBestFit(int requiredSize)
+    {
         int bestSize = INT_MAX;
         int bestStart = 0;
 
-        for (const auto& block : blocks) {
+        for (const auto &block : blocks)
+        {
             int gap = (block.startAddress - bestStart) - requiredSize;
-            if (gap >= 0 && gap < bestSize) {
+            if (gap >= 0 && gap < bestSize)
+            {
                 bestSize = gap;
                 bestStart = block.startAddress;
             }
@@ -135,13 +290,16 @@ public:
         return bestStart;
     }
 
-    int findWorstFit(int requiredSize) {
+    int findWorstFit(int requiredSize)
+    {
         int worstSize = -1;
         int worstStart = 0;
 
-        for (const auto& block : blocks) {
+        for (const auto &block : blocks)
+        {
             int gap = (block.startAddress - worstStart) - requiredSize;
-            if (gap > worstSize) {
+            if (gap > worstSize)
+            {
                 worstSize = gap;
                 worstStart = block.startAddress;
             }
@@ -149,23 +307,29 @@ public:
         return worstStart;
     }
 
-    void deleteFile(const std::string& fileID) {
-        auto it = std::remove_if(blocks.begin(), blocks.end(), [&fileID](const MemoryBlock& block) {
-            return block.fileID == fileID;
-        });
+    void deleteFile(const std::string &fileID)
+    {
+        auto it = std::remove_if(blocks.begin(), blocks.end(), [&fileID](const MemoryBlock &block)
+                                 { return block.fileID == fileID; });
 
-        if (it != blocks.end()) {
-            it->release();  // Liberar el bloque
+        if (it != blocks.end())
+        {
+            it->release(); // Liberar el bloque
             blocks.erase(it, blocks.end());
             std::cout << "Archivo " << fileID << " eliminado.\n";
-        } else {
+        }
+        else
+        {
             std::cout << "Archivo " << fileID << " no encontrado.\n";
         }
     }
 
-    void overwriteFile(const std::string& fileID, const std::string& newContent) {
-        for (auto& block : blocks) {
-            if (block.fileID == fileID) {
+    void overwriteFile(const std::string &fileID, const std::string &newContent)
+    {
+        for (auto &block : blocks)
+        {
+            if (block.fileID == fileID)
+            {
                 block.content = newContent;
                 std::cout << "Archivo " << fileID << " sobrescrito.\n";
                 return;
@@ -174,12 +338,14 @@ public:
         std::cout << "Archivo " << fileID << " no encontrado para sobrescribir.\n";
     }
 
-    void saveToUnisFormat(const std::string& fileName) {
+    void saveToUnisFormat(const std::string &fileName)
+    {
         std::ofstream outFile(fileName + ".unis");
 
         outFile << totalSize << " " << pageSize << "\n";
 
-        for (const auto& block : blocks) {
+        for (const auto &block : blocks)
+        {
             outFile << block.startAddress << " "
                     << block.size << " "
                     << block.fileID << " "
@@ -191,50 +357,67 @@ public:
     }
 };
 
-void testBestFitAllocation() {
+void testBestFitAllocation()
+{
     MemoryManager manager(1024, 64, BEST_FIT);
     manager.loadFile("testFile1.txt", TEXT);
     manager.loadFile("testFile2.txt", TEXT);
     assert(manager.blocks[0].startAddress <= manager.blocks[1].startAddress && "Best Fit allocation failed");
 }
 
-void testWorstFitAllocation() {
+void testWorstFitAllocation()
+{
     MemoryManager manager(1024, 64, WORST_FIT);
     manager.loadFile("testFile1.txt", TEXT);
     manager.loadFile("testFile2.txt", TEXT);
     assert(manager.blocks[0].startAddress <= manager.blocks[1].startAddress && "Worst Fit allocation failed");
 }
 
-void testFIFOPagination() {
-    MemoryManager manager(1024, 64, BEST_FIT);
+void testFIFOPagination()
+{
+    MemoryManager manager(1024, 64, FIFO);
     manager.loadFile("testFile1.txt", TEXT);
     manager.loadFile("testFile2.txt", TEXT);
-    manager.pageQueue.pop();  // Simulando FIFO
+    manager.pageQueue.pop(); // Simulando FIFO
     assert(manager.pageQueue.size() == 1 && "FIFO Pagination failed");
 }
 
-int main() {
+void testLRUPagination()
+{
+    MemoryManager manager(1024, 64, LRU);
+    manager.loadFile("testFile1.txt", TEXT);
+    manager.loadFile("testFile2.txt", TEXT);
+    manager.pageQueue.pop(); // Simulando FIFO
+    assert(manager.pageQueue.size() == 1 && "LRU Pagination failed");
+}
+
+int main()
+{
     // Ejecutando pruebas
     testBestFitAllocation();
     testWorstFitAllocation();
     testFIFOPagination();
+    testLRUPagination();
     std::cout << "Todas las pruebas pasaron con éxito." << std::endl;
 
     MemoryManager manager(1024, 64, BEST_FIT);
-    manager.loadFile("textFile.txt", TEXT);  // Asegúrate de que textFile.txt exista
-    manager.loadFile("imageFile.png", BINARY);  // Asegúrate de que imageFile.png exista
+    manager.loadFile("textFile.txt", TEXT);    // Asegúrate de que textFile.txt exista
+    manager.loadFile("imageFile.png", BINARY); // Asegúrate de que imageFile.png exista
 
     manager.deleteFile("textFile.txt");
     manager.overwriteFile("imageFile.png", "Nuevo contenido");
 
-    while (true) {
+    while (true)
+    {
         std::cout << "\n--- Menú ---\n";
         std::cout << "1. Cargar archivo de texto\n";
         std::cout << "2. Cargar archivo binario\n";
         std::cout << "3. Eliminar archivo\n";
         std::cout << "4. Sobrescribir archivo\n";
         std::cout << "5. Guardar en formato .unis\n";
-        std::cout << "6. Salir\n";
+        std::cout << "6. Eliminar bloque\n";
+        std::cout << "7. Sobreescribir bloque\n";
+        std::cout << "8. Salir\n";
 
         int choice;
         std::cout << "Elige una opción: ";
@@ -242,45 +425,64 @@ int main() {
 
         std::string fileName, content;
 
-        switch (choice) {
-            case 1:
-                std::cout << "Nombre del archivo de texto para cargar: ";
-                std::cin >> fileName;
-                manager.loadFile(fileName, TEXT);
-                break;
+        std::string newContent;
 
-            case 2:
-                std::cout << "Nombre del archivo binario para cargar: ";
-                std::cin >> fileName;
-                manager.loadFile(fileName, BINARY);
-                break;
+        switch (choice)
+        {
+        case 1:
+            std::cout << "Nombre del archivo de texto para cargar: ";
+            std::cin >> fileName;
+            manager.loadFile(fileName, TEXT);
+            break;
 
-            case 3:
-                std::cout << "Nombre del archivo para eliminar: ";
-                std::cin >> fileName;
-                manager.deleteFile(fileName);
-                break;
+        case 2:
+            std::cout << "Nombre del archivo binario para cargar: ";
+            std::cin >> fileName;
+            manager.loadFile(fileName, BINARY);
+            break;
 
-            case 4:
-                std::cout << "Nombre del archivo para sobrescribir: ";
-                std::cin >> fileName;
-                std::cout << "Nuevo contenido: ";
-                std::cin >> content;
-                manager.overwriteFile(fileName, content);
-                break;
+        case 3:
+            std::cout << "Nombre del archivo para eliminar: ";
+            std::cin >> fileName;
+            manager.deleteFile(fileName);
+            break;
 
-            case 5:
-                std::cout << "Nombre del archivo donde guardar en formato .unis: ";
-                std::cin >> fileName;
-                manager.saveToUnisFormat(fileName);
-                break;
+        case 4:
+            std::cout << "Nombre del archivo para sobrescribir: ";
+            std::cin >> fileName;
+            std::cout << "Nuevo contenido: ";
+            std::cin >> content;
+            manager.overwriteFile(fileName, content);
+            break;
 
-            case 6:
-                std::cout << "Saliendo...\n";
-                return 0;
+        case 5:
+            std::cout << "Nombre del archivo donde guardar en formato .unis: ";
+            std::cin >> fileName;
+            manager.saveToUnisFormat(fileName);
+            break;
 
-            default:
-                std::cout << "Opción no válida.\n";
+        case 6:
+            int deleteAddress;
+            std::cout << "Dirección del bloque a eliminar: ";
+            std::cin >> deleteAddress;
+            manager.deleteBlock(deleteAddress);
+            break;
+
+        case 7:
+            int overwriteAddress;
+            std::cout << "Dirección del bloque a sobrescribir: ";
+            std::cin >> overwriteAddress;
+            std::cout << "Nuevo contenido: ";
+            std::cin >> newContent;
+            manager.overwriteBlock(overwriteAddress, newContent);
+            break;
+
+        case 8:
+            std::cout << "Saliendo...\n";
+            return 0;
+
+        default:
+            std::cout << "Opción no válida.\n";
         }
     }
 
